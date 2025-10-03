ec98a3cbd0235d2ef568d03a9a257623d23bf8b7
DenisShabanov <Shabik35@yandex.ru>
2025-10-03 13:17:49 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Denis Shabanov on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
